4445c267153d166c8b57b2d970357b9cebea0c9d
Debjit Chakraborty <zxyron0@gmail.com>
2020-09-13 23:46:00 +0530

    Separate featured albums widget

--- lib/ui/homePage.dart
+++ lib/ui/homePage.dart
@@ -1,24 +1,16 @@
-import 'dart:io';
 import 'dart:ui';
-
-import 'package:audiotagger/audiotagger.dart';
-import 'package:audiotagger/models/tag.dart';
+import 'package:Musify/model/downloadengine.dart';
+import 'package:Musify/ui/widgets/featuredplaylist.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:ext_storage/ext_storage.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_media_notification/flutter_media_notification.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:gradient_widgets/gradient_widgets.dart';
-import 'package:http/http.dart' as http;
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:Musify/API/saavn.dart';
 import 'package:Musify/music.dart';
 import 'package:Musify/style/appColors.dart';
 import 'package:Musify/ui/aboutPage.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:progress_dialog/progress_dialog.dart';
 
 class Musify extends StatefulWidget {
   @override
@@ -30,6 +22,8 @@ class Musify extends StatefulWidget {
 class AppState extends State<Musify> {
   TextEditingController searchBar = TextEditingController();
   bool fetchingSongs = false;
+
+  DownloadEngine downloadEngine = new DownloadEngine();
 
   getLists() async {
     var list = await getFeaturedPlaylists();
@@ -94,140 +88,6 @@ class AppState extends State<Musify> {
         builder: (context) => AudioApp(),
       ),
     );
-  }
-
-  downloadSong(id) async {
-    String filepath;
-    String filepath2;
-    var status = await Permission.storage.status;
-    if (status.isUndetermined || status.isDenied) {
-      // code of read or write file in external storage (SD card)
-      // You can request multiple permissions at once.
-      Map<Permission, PermissionStatus> statuses = await [
-        Permission.storage,
-      ].request();
-      debugPrint(statuses[Permission.storage].toString());
-    }
-    status = await Permission.storage.status;
-    await fetchSongDetails(id);
-    if (status.isGranted) {
-      ProgressDialog pr = ProgressDialog(context);
-      pr = ProgressDialog(
-        context,
-        type: ProgressDialogType.Normal,
-        isDismissible: false,
-        showLogs: false,
-      );
-
-      pr.style(
-        backgroundColor: Color(0xff263238),
-        elevation: 4,
-        textAlign: TextAlign.left,
-        progressTextStyle: TextStyle(color: Colors.white),
-        message: "Downloading " + title,
-        messageTextStyle: TextStyle(color: accent),
-        progressWidget: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(accent),
-          ),
-        ),
-      );
-      await pr.show();
-
-      final filename = title + ".m4a";
-      final artname = title + "_artwork.jpg";
-      //Directory appDocDir = await getExternalStorageDirectory();
-      String dlPath = await ExtStorage.getExternalStoragePublicDirectory(
-          ExtStorage.DIRECTORY_MUSIC);
-      await File(dlPath + "/" + filename)
-          .create(recursive: true)
-          .then((value) => filepath = value.path);
-      await File(dlPath + "/" + artname)
-          .create(recursive: true)
-          .then((value) => filepath2 = value.path);
-      debugPrint('Audio path $filepath');
-      debugPrint('Image path $filepath2');
-      if (has_320 == "true") {
-        kUrl = rawkUrl.replaceAll("_96.mp4", "_320.mp4");
-        final client = http.Client();
-        final request = http.Request('HEAD', Uri.parse(kUrl))
-          ..followRedirects = false;
-        final response = await client.send(request);
-        debugPrint(response.statusCode.toString());
-        kUrl = (response.headers['location']);
-        debugPrint(rawkUrl);
-        debugPrint(kUrl);
-        final request2 = http.Request('HEAD', Uri.parse(kUrl))
-          ..followRedirects = false;
-        final response2 = await client.send(request2);
-        if (response2.statusCode != 200) {
-          kUrl = kUrl.replaceAll(".mp4", ".mp3");
-        }
-      }
-      var request = await HttpClient().getUrl(Uri.parse(kUrl));
-      var response = await request.close();
-      var bytes = await consolidateHttpClientResponseBytes(response);
-      File file = File(filepath);
-
-      var request2 = await HttpClient().getUrl(Uri.parse(image));
-      var response2 = await request2.close();
-      var bytes2 = await consolidateHttpClientResponseBytes(response2);
-      File file2 = File(filepath2);
-
-      await file.writeAsBytes(bytes);
-      await file2.writeAsBytes(bytes2);
-      debugPrint("Started tag editing");
-
-      final tag = Tag(
-        title: title,
-        artist: artist,
-        artwork: filepath2,
-        album: album,
-        lyrics: lyrics,
-        genre: null,
-      );
-
-      debugPrint("Setting up Tags");
-      final tagger = Audiotagger();
-      await tagger.writeTags(
-        path: filepath,
-        tag: tag,
-      );
-      await Future.delayed(const Duration(seconds: 1), () {});
-      await pr.hide();
-
-      if (await file2.exists()) {
-        await file2.delete();
-      }
-      debugPrint("Done");
-      Fluttertoast.showToast(
-          msg: "Download Complete!",
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.BOTTOM,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.black,
-          textColor: Color(0xff61e88a),
-          fontSize: 14.0);
-    } else if (status.isDenied || status.isPermanentlyDenied) {
-      Fluttertoast.showToast(
-          msg: "Storage Permission Denied!\nCan't Download Songs",
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.BOTTOM,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.black,
-          textColor: Color(0xff61e88a),
-          fontSize: 14.0);
-    } else {
-      Fluttertoast.showToast(
-          msg: "Permission Error!",
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.values[50],
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.black,
-          textColor: Color(0xff61e88a),
-          fontSize: 14.0);
-    }
   }
 
   @override
@@ -531,8 +391,10 @@ class AppState extends State<Musify> {
                                     trailing: IconButton(
                                       color: accent,
                                       icon: Icon(MdiIcons.downloadOutline),
-                                      onPressed: () => downloadSong(
-                                          searchedList[index]["id"]),
+                                      onPressed: () =>
+                                          downloadEngine.downloadSong(
+                                              searchedList[index]["id"],
+                                              context),
                                     ),
                                   ),
                                 ],
@@ -596,50 +458,7 @@ class AppState extends State<Musify> {
                             );
                           },
                         ),
-                        FutureBuilder(
-                          future: getFeaturedPlaylists(),
-                          builder: (context, snapshot) {
-                            if (snapshot.hasData) {
-                              var list = snapshot.data;
-                              return Container(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.35,
-                                child: ListView.builder(
-                                  itemCount: 10,
-                                  scrollDirection: Axis.horizontal,
-                                  itemBuilder: (context, index) {
-                                    return Container(
-                                      padding: EdgeInsets.all(10.0),
-                                      height: 200,
-                                      width: 200,
-                                      child: Column(
-                                        children: [
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(10.0),
-                                            child: CachedNetworkImage(
-                                                imageUrl: list[index].imageUrl),
-                                          ),
-                                          SizedBox(height: 10),
-                                          Text(
-                                            list[index].name,
-                                            textAlign: TextAlign.center,
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    );
-                                  },
-                                ),
-                              );
-                            } else {
-                              return Center(child: CircularProgressIndicator());
-                            }
-                          },
-                        ),
+                        FeaturedPlayListWidget(),
                       ],
                     ),
             ],
---
+++ lib/ui/widgets/featuredplaylist.dart
@@ -0,0 +1,73 @@
+import 'package:Musify/API/saavn.dart';
+import 'package:Musify/style/appColors.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedPlayListWidget extends StatelessWidget {
+  const FeaturedPlayListWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+          child: Text(
+            'Featured PlayLists',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 22,
+              color: accent,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        FutureBuilder(
+          future: getFeaturedPlaylists(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var list = snapshot.data;
+              return Container(
+                height: MediaQuery.of(context).size.height * 0.35,
+                child: ListView.builder(
+                  itemCount: 10,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: EdgeInsets.all(10.0),
+                      height: 200,
+                      width: 200,
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: CachedNetworkImage(
+                                imageUrl: list[index].imageUrl),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            list[index].name,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
