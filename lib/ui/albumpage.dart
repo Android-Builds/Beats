023e01e3e2f7ff53907ca606c6f39f6329b7fd4b
xayron <zxyron0@gmail.com>
2020-10-08 09:23:39 +0530

    Add more info in playlist page

--- lib/ui/albumpage.dart
+++ lib/ui/albumpage.dart
@@ -24,7 +24,8 @@ class _AlbumPageState extends State<AlbumPage> {
   );
 
   TextStyle small = TextStyle(fontSize: 10.0, fontWeight: FontWeight.w200);
-  TextStyle medium = TextStyle(fontSize: 12.0, fontWeight: FontWeight.w300);
+  TextStyle medium = TextStyle(
+      fontSize: 13.0, fontWeight: FontWeight.w300, color: Colors.grey[680]);
 
   @override
   void initState() {
@@ -35,141 +36,168 @@ class _AlbumPageState extends State<AlbumPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 0.0),
-          child: ListView(
-            children: [
-              Column(
-                //mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Hero(
-                    tag: featuredPlaylists[index]['listname'],
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10.0),
-                      child: CachedNetworkImage(
-                        height: 200,
-                        width: 200,
-                        imageUrl: featuredPlaylists[index]['image'],
-                      ),
+        child: ListView(
+          children: [
+            SizedBox(height: 20.0),
+            Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: featuredPlaylists[index]['listname'],
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: CachedNetworkImage(
+                      height: 200,
+                      width: 200,
+                      imageUrl: featuredPlaylists[index]['image'],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        top: 5.0, left: 20.0, right: 20.0, bottom: 5.0),
-                    child: Text(featuredPlaylists[index]['listname'],
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22.0,
-                            fontWeight: FontWeight.w400)),
-                  ),
-                ],
-              ),
-              FutureBuilder(
-                future: getPlaylistDetails(featuredPlaylists[index]['listid']),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    var songdata = snapshot.data['songs'];
-                    var data = snapshot.data;
-                    //print(data);
-                    return Column(
-                      children: [
-                        Row(
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 5.0, left: 20.0, right: 20.0, bottom: 5.0),
+                  child: Text(featuredPlaylists[index]['listname'],
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.w400)),
+                ),
+              ],
+            ),
+            FutureBuilder(
+              future: getPlaylistDetails(featuredPlaylists[index]['listid']),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  var songdata = snapshot.data['songs'];
+                  int duration = 0;
+                  songdata.forEach((element) {
+                    duration += int.parse(element['duration']);
+                  });
+                  var data = snapshot.data;
+                  duration = Duration(seconds: duration).inMinutes;
+                  //print(data);
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 90.0,
+                          vertical: 5.0,
+                        ),
+                        child: Column(
                           children: [
+                            SizedBox(height: 5.0),
                             Text(
-                                'Language: ' +
-                                    featuredPlaylists[index]['language'],
+                                featuredPlaylists[index]['language']
+                                        .toString()
+                                        //Make the first letter capital
+                                        .replaceFirst(
+                                            featuredPlaylists[index]['language']
+                                                .toString()
+                                                .substring(0, 1),
+                                            featuredPlaylists[index]['language']
+                                                .toString()
+                                                .substring(0, 1)
+                                                .toUpperCase()) +
+                                    //Separator
+                                    ' • ' +
+                                    data['list_count'].toString() +
+                                    ' Songs • ' +
+                                    //Convert duration to minute,
+                                    //then separate out hour and minute
+                                    (duration / 60 < 1
+                                        ? ''
+                                        : (duration ~/ 60).toString() +
+                                            ' hr ') +
+                                    (duration % 60 == 0
+                                        ? ''
+                                        : (duration % 60).toString() + ' min'),
                                 style: medium),
-                            Spacer(),
+                            SizedBox(height: 5.0),
                             Text(
-                                featuredPlaylists[index]['song_count'] +
-                                    ' Songs',
+                                (data['fan_count'] / 1000).toStringAsFixed(1) +
+                                    'K fans' +
+                                    ' • ' +
+                                    (int.parse(data['follower_count']) / 1000)
+                                        .toStringAsFixed(1) +
+                                    'K followers',
                                 style: medium),
                           ],
                         ),
-                        Row(
-                          children: [
-                            // Text('Year: ' + data['year'], style: medium),
-                            Spacer(),
-                            Text(
-                                'Language: ' +
-                                    featuredPlaylists[index]['song_count'],
-                                style: medium),
-                          ],
-                        ),
-                        SizedBox(height: 20.0),
-                        ListView.builder(
-                          shrinkWrap: true,
-                          physics: NeverScrollableScrollPhysics(),
-                          itemCount: songdata.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return FlatButton(
-                              onPressed: () => Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => NowPlaying(
-                                          songId:
-                                              songdata[index]['id'].toString(),
-                                          newSong: !(currentSongId ==
-                                              songdata[index]['id']
-                                                  .toString())))),
-                              child: Container(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 20.0, vertical: 15.0),
-                                child: Row(
-                                  children: [
-                                    Container(
-                                      height: 50.0,
-                                      width: 50.0,
-                                      decoration: BoxDecoration(
-                                        borderRadius:
-                                            BorderRadius.circular(10.0),
-                                        image: DecorationImage(
-                                          image: CachedNetworkImageProvider(
-                                              songdata[index]['image']),
+                      ),
+                      SizedBox(height: 20.0),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: songdata.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return FlatButton(
+                            onPressed: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => NowPlaying(
+                                        songId:
+                                            songdata[index]['id'].toString(),
+                                        newSong: !(currentSongId ==
+                                            songdata[index]['id']
+                                                .toString())))),
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 20.0, vertical: 15.0),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: 50.0,
+                                    width: 50.0,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      image: DecorationImage(
+                                        image: CachedNetworkImageProvider(
+                                            songdata[index]['image']),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 10),
+                                  SizedBox(
+                                    width: 200.0,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          songdata[index]['song'],
+                                          maxLines: 2,
+                                          overflow: TextOverflow.clip,
+                                          style: TextStyle(fontSize: 15.0),
                                         ),
-                                      ),
+                                        SizedBox(height: 5.0),
+                                        Text(
+                                          songdata[index]['album'] +
+                                              ' - ' +
+                                              songdata[index]['singers'],
+                                          maxLines: 2,
+                                          overflow: TextOverflow.clip,
+                                          style: small,
+                                        ),
+                                      ],
                                     ),
-                                    SizedBox(width: 10),
-                                    SizedBox(
-                                      width: 200.0,
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            songdata[index]['song'],
-                                            maxLines: 2,
-                                            overflow: TextOverflow.clip,
-                                            style: TextStyle(fontSize: 15.0),
-                                          ),
-                                          SizedBox(height: 5.0),
-                                          Text(
-                                            songdata[index]['album'] +
-                                                ' - ' +
-                                                songdata[index]['singers'],
-                                            maxLines: 2,
-                                            overflow: TextOverflow.clip,
-                                            style: small,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
-                            );
-                          },
-                        ),
-                      ],
-                    );
-                  } else {
-                    return Center(child: CircularProgressIndicator());
-                  }
-                },
-              ),
-            ],
-          ),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                } else {
+                  return Container(
+                      height: MediaQuery.of(context).size.height / 2,
+                      child: Center(child: CircularProgressIndicator()));
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
